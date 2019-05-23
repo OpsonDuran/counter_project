707eda95e03798086305861e3892b48447eb34ac
OpsonDuran <iccarmandoduran@gmail.com>
2019-05-23 01:33:34 -0500

    add widget MaterialApp and Text in propertie of widget Material App for return in MyApp()

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ void main() {
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Text('hello world') ,
+    );
   }
-
 }
