27260a8174a7bc3aeeb3e91da04782c30ee94567
OpsonDuran <iccarmandoduran@gmail.com>
2019-05-23 01:42:03 -0500

    add new folder src and new file app.dart for a better organization the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child:Text('hello world')
-        ) ,
-    );
-  }
-}
