d55b9de7c39d3dcb7aa6c849d9d748d85066e88c
OpsonDuran <iccarmandoduran@gmail.com>
2019-05-25 22:22:02 -0500

    add to var final for define TextStyle in widgets Text and replace in this widgets

--- lib/src/widget/home_page.dart
+++ lib/src/widget/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  final  styleText = TextStyle(fontSize: 25,fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('Tap Counter App'),
@@ -13,8 +15,8 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Number of taps',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
-            Text('0',style: TextStyle(fontSize: 25),),
+            Text('Number of taps',style: styleText),
+            Text('0',style: styleText,),
           ],
         ),
       ),
