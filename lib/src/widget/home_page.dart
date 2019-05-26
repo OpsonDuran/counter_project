b099bbeeff2d63c112407f1cd879891af573023c
OpsonDuran <iccarmandoduran@gmail.com>
2019-05-25 22:16:07 -0500

    add colum in homepage widget and style in text

--- lib/src/widget/home_page.dart
+++ lib/src/widget/home_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(appBar: AppBar(
-      title: Text('Hello world'),
-    ),
-    body: Center(
-      child: Text('Hello World'),
-    ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tap Counter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Number of taps',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+            Text('0',style: TextStyle(fontSize: 25),),
+          ],
+        ),
+      ),
+    );
   }
 }
